158d47c804004ef4f677e75d278982885c30c78e
JanF01 <jan.fiolka44@gmail.com>
2023-07-27 11:46:34 +0000

    fix: change of file directory

--- classifier/StreetNumericClassifier.js
+++ classifier/StreetNumericClassifier.js
@@ -1,6 +1,6 @@
 const PhraseClassifier = require('./super/PhraseClassifier')
 const StreetNumericClassification = require('../classification/StreetNumericClassification')
-const pelias = require('../resources/pelias/libpostal')
+const pelias = require('../resources/pelias/dictionaries/libpostal')
 
 // dictionaries sourced from the libpostal project
 // see: https://github.com/openvenues/libpostal
